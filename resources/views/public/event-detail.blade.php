26f8bb95c1eeb1bc5ebeec4d69ab37d32d27b94a
Itok toni laksono <itok.toni@gmail.com>
2024-10-05 22:50:59 +0700

    update race

--- resources/views/public/event-detail.blade.php
+++ resources/views/public/event-detail.blade.php
@@ -34,89 +34,97 @@
     <div class="tf-widget-events">
         <div class="themeflat-container">
 
-            <div id="accordion">
-                <div class="card">
-                    <div class="card-header" id="headingOne">
-                        <h5 class="mb-0">
-                            <button class="btn" data-bs-toggle="collapse" data-bs-target="#collapseOne"
-                                aria-expanded="true" aria-controls="collapseOne">
-                                <h3>Race Detail</h3>
-                            </button>
-                        </h5>
-                    </div>
+            <div class="row">
+                <div class="col-md-10 mx-auto">
 
-                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                        <div class="card-body">
-                            <div class="image-event">
-                                <img src="{{ asset('storage/files/event/' . $event->event_detail) }}" alt="Image Events"
-                                    class="wow fadeInLeft animated">
+                    <div id="accordion">
+                        <div class="card">
+                            <div class="card-header header-event" id="headingOne">
+                                <h5 class="mb-0">
+                                    <button class="btn" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                                        aria-expanded="true" aria-controls="collapseOne">
+                                        <h5>Race Detail</h5>
+                                    </button>
+                                </h5>
+                            </div>
+
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="image-event">
+                                        <img src="{{ asset('storage/files/event/' . $event->event_detail) }}" alt="Image Events"
+                                            class="wow fadeInLeft animated">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="card">
-                    <div class="card-header" id="headingTwo">
-                        <h5 class="mb-0">
-                            <button class="btn collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo"
-                                aria-expanded="false" aria-controls="collapseTwo">
-                                <h3>Course Map</h3>
-                            </button>
-                        </h5>
-                    </div>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                        <div class="card-body">
-                            <div class="image-event">
-                                <img src="{{ asset('storage/files/event/' . $event->event_map) }}" alt="Image Events"
-                                    class="wow fadeInLeft animated">
+                        <div class="card">
+                            <div class="card-header header-event" id="headingTwo">
+                                <h5 class="mb-0">
+                                    <button class="btn collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo"
+                                        aria-expanded="false" aria-controls="collapseTwo">
+                                        <h5>Course Map</h5>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="image-event">
+                                        <img src="{{ asset('storage/files/event/' . $event->event_map) }}" alt="Image Events"
+                                            class="wow fadeInLeft animated">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="card">
-                    <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree"
-                                aria-expanded="false" aria-controls="collapseThree">
-                                <h3>
-                                    Mandatory
-                                </h3>
-                            </button>
-                        </h5>
-                    </div>
-                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                        <div class="card-body">
-                            <div class="image-event">
-                                <img src="{{ asset('storage/files/event/' . $event->event_mandatory) }}" alt="Image Events"
-                                    class="wow fadeInLeft animated">
+                        <div class="card">
+                            <div class="card-header header-event" id="headingThree">
+                                <h5 class="mb-0">
+                                    <button class="btn collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree"
+                                        aria-expanded="false" aria-controls="collapseThree">
+                                        <h5>
+                                            Mandatory
+                                        </h5>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="image-event">
+                                        <img src="{{ asset('storage/files/event/' . $event->event_mandatory) }}" alt="Image Events"
+                                            class="wow fadeInLeft animated">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
 
-                <div class="card">
-                    <div class="card-header" id="headingThree">
-                        <h5 class="mb-0">
-                            <button class="btn collapsed" data-bs-toggle="collapse" data-bs-target="#collapse4"
-                                aria-expanded="false" aria-controls="collapse4">
-                                <h3>
-                                    Roundown
-                                </h3>
-                            </button>
-                        </h5>
-                    </div>
-                    <div id="collapse4" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                        <div class="card-body">
-                            <div class="image-event">
-                                <img src="{{ asset('storage/files/event/' . $event->event_roundown) }}" alt="Image Events"
-                                    class="wow fadeInLeft animated">
+                        <div class="card">
+                            <div class="card-header header-event" id="headingThree">
+                                <h5 class="mb-0">
+                                    <button class="btn collapsed" data-bs-toggle="collapse" data-bs-target="#collapse4"
+                                        aria-expanded="false" aria-controls="collapse4">
+                                        <h5>
+                                            Roundown
+                                        </h5>
+                                    </button>
+                                </h5>
+                            </div>
+                            <div id="collapse4" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="image-event">
+                                        <img src="{{ asset('storage/files/event/' . $event->event_roundown) }}" alt="Image Events"
+                                            class="wow fadeInLeft animated">
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
 
-            </div>
+                    </div>
+
+                </div>
+              </div>
+
+
 
             <div class="row">
                 <div class="col-md-12">
