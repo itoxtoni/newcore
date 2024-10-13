419f4ab7a5671f85be1e0a04df153b93e15d956a
itoktoni <itok.toni@gmail.com>
2024-10-13 19:11:27 +0700

    update login

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -11,6 +11,11 @@ class PublicController extends Controller
 {
     public function index()
     {
+        if(env('APP_AUTH', true))
+        {
+            return redirect()->to('/login');
+        }
+
         return view('homepage');
     }
 
