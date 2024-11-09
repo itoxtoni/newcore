d0b48c40055745a02baaedba32fb60bee57a1bae
itoktoni <itok.toni@gmail.com>
2024-11-09 16:16:57 +0700

    update disable form

--- resources/views/components/action.blade.php
+++ resources/views/components/action.blade.php
@@ -2,46 +2,43 @@
     'label' => null,
     'model' => null,
     'form' => null,
-    'override' => false
-    ])
+    'override' => false,
+])
 
-    @php
-        $attributes = $attributes->class([
-        'button button-action',
-        ])->merge([
+@php
+    $attributes = $attributes->class(['button button-action'])->merge([
         //
-        ]);
+    ]);
 
-        if($form == null){
+    if ($form == null) {
         $form = 'table';
-        }
-    @endphp
+    }
+@endphp
 
-    @section('action')
-    @if ($form != false)
+@section('action')
+    @if ($form != 'disable')
+        <div {{ $attributes }}>
+            @if ($form == 'table')
+                <input class="btn-check-m d-lg-none" type="checkbox">
+                @can(ACTION_EMPTY)
+                    <x-button onclick="return confirm('Apakah anda yakin ingin menghapus ?')" name="delete" type="submit"
+                        color="danger" label="Kosongkan" />
+                @endcan
+                @can(ACTION_CREATE)
+                    <x-button :module="ACTION_CREATE" color="success" label="Buat" />
+                @endcan
+            @elseif($form == 'print')
+                @can(ACTION_PRINT)
+                    <x-button name="type" value="report" type="submit" label="Print" />
+                @endcan
+            @elseif($form == 'form')
+                @can(ACTION_CREATE || ACTION_UPDATE)
+                    <x-button type="submit" label="Simpan" />
+                @endcan
+            @endif
 
-    <div {{ $attributes }}>
-        @if($form == 'table')
-            <input class="btn-check-m d-lg-none" type="checkbox">
-            @can(ACTION_EMPTY)
-            <x-button onclick="return confirm('Apakah anda yakin ingin menghapus ?')" name="delete" type="submit" color="danger" label="Kosongkan"/>
-            @endcan
-            @can(ACTION_CREATE)
-            <x-button :module="ACTION_CREATE" color="success" label="Buat" />
-            @endcan
-        @elseif($form == 'print')
-            @can(ACTION_PRINT)
-            <x-button name="type" value="report" type="submit" label="Print" />
-            @endcan
-        @elseif($form == 'form')
-            @can(ACTION_CREATE || ACTION_UPDATE)
-            <x-button type="submit" label="Simpan" />
-            @endcan
-        @endif
-
-        {{ $slot }}
-    </div>
-
+            {{ $slot }}
+        </div>
     @endif
 
-    @endsection
+@endsection
