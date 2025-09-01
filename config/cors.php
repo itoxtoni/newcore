e7e68608a366d9aa4d7f8656deac0386b6cdd94f
itok toni laksono <itok.toni@gmail.com>
2025-09-01 15:42:44 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://localhost',
+        'capacitor://localhost',
+        'https://newcore.nexeratech.co.id',
+    ],
 
     'allowed_origins_patterns' => [],
 
