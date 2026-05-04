e0930eb92b9d1a5187363e3f03912ec9b3d8841a
itok toni <itok.toni@gmail.com>
2026-05-04 11:11:42 +0700

    update dashboard

--- app/Charts/Dashboard.php
+++ app/Charts/Dashboard.php
@@ -19,9 +19,9 @@ class Dashboard
             ->setTitle('San Francisco vs Boston.')
             ->setSubtitle('Wins during season 2021.')
             ->setGrid()
-            ->addData('San Francisco', [6, 9, 3, 4, 10, 8])
-            ->addData('Boston', [7, 3, 8, 2, 6, 4])
-            ->addData('Wales', [7, 3, 8, 2, 6, 4])
+            ->addData( [6, 9, 3, 4, 10, 8], 'San Francisco')
+            ->addData([7, 3, 8, 2, 6, 4], 'Boston')
+            ->addData([7, 3, 8, 2, 6, 4], 'Wales')
             ->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
     }
 }
